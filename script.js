c0a552d3a583bb435cfe0021f17f90fd3bb2f8d1
fstipo <fstipanov@gmail.com>
2022-05-17 16:52:58 +0200

    object prototype

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 
   // Never do this
-  this.calcAge = function () {
-    console.log(2022 - this.birthYear);
-  };
+  // this.calcAge = function () {
+  //   console.log(2022 - this.birthYear);
+  // };
 };
 
 const franko = new Person('Franko', 1977);
@@ -26,6 +26,59 @@ console.log(mathilda);
 console.log(olga);
 const jill = 'Jill';
 
+// instanceof
 console.log(franko instanceof Person);
 console.log(jill instanceof Person);
-console.log(franko.calcAge());
+
+// Prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2022 - this.birthYear);
+};
+
+franko.calcAge();
+mathilda.calcAge();
+olga.calcAge();
+
+console.log('---------------');
+console.log(franko.__proto__);
+console.log(franko.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(franko));
+console.log(Person.prototype.isPrototypeOf(olga));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+//Person.prototype zamislimo kao prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo sapiens';
+console.log(franko);
+console.log(franko.species, olga.species);
+
+console.log(franko.hasOwnProperty('firstName'));
+console.log(franko.hasOwnProperty('species'));
+
+console.log(franko.__proto__);
+console.log(franko.__proto__.__proto__);
+console.log(franko.__proto__.__proto__.__proto__);
+console.log(franko.valueOf());
+console.log(franko);
+
+console.log(Person.prototype.constructor);
+console.dir(Person.prototype.constructor);
+
+let arr = [1, 2, 3, 1, 2, 3, 4]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+console.log(arr.__proto__.__proto__.__proto__);
+
+// add new method to array
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+console.dir(x => x + 1);
