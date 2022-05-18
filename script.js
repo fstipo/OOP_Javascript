6fdf45a5596123bbdd4897a20215801f4f0141a3
fstipo <fstipanov@gmail.com>
2022-05-18 17:01:17 +0200

    getter and setter , static method

--- script.js
+++ script.js
@@ -121,26 +121,110 @@
 // Mazda.accelerate();
 // Mazda.accelerate();
 
-// Procedural code
-let baseSalary = 30000;
-let overtime = 10;
-let rate = 20;
+// // Procedural code
+// let baseSalary = 30000;
+// let overtime = 10;
+// let rate = 20;
 
-const getWage = function (baseSalary, overtime, rate) {
-  return baseSalary + overtime * rate;
-};
+// const getWage = function (baseSalary, overtime, rate) {
+//   return baseSalary + overtime * rate;
+// };
 
-console.log(getWage(baseSalary, overtime, rate));
+// console.log(getWage(baseSalary, overtime, rate));
 
-// OOP
-const employee = {
-  baseSalary: 40000,
-  overtime: 10,
-  rate: 20,
+// // OOP
+// const employee = {
+//   baseSalary: 40000,
+//   overtime: 10,
+//   rate: 20,
 
-  getWage: function () {
-    return this.baseSalary + this.overtime * this.rate;
+//   getWage: function () {
+//     return this.baseSalary + this.overtime * this.rate;
+//   },
+// };
+
+// console.log(employee.getWage());
+// let a;
+// //class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to prototype.property
+  // instance methods
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2022 - this.birthYear;
+  }
+
+  //set property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${this._fullName} is not full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // static method
+  static hey() {
+    console.log('Hello 👍');
+  }
+}
+
+PersonCl.hey();
+// //class expression
+// // const PersonCl = class{}
+
+const jessica = new PersonCl('Jessica Black', 1982);
+console.log(`${jessica.fullName} have ${jessica.age} years`);
+
+// jessica.calcAge();
+// console.log(jessica.__proto__ === PersonCl.prototype);
+
+// // PersonCl.prototype.greet = function () {
+// //   console.log(`Hey ${this.firstName}`);
+// // };
+
+// jessica.greet();
+
+/* Rules about classes
+1. Classes are NOT hoisted / iako su funcion declaration
+ne možemo ih pozvati prije nego ih deklariramo
+2. First class citizens - možemo ih proslijediti u funkciju i isto tako return
+from functions
+3. Classes are executed in strict mode
+*/
+
+// getters and setters
+const account = {
+  owner: 'Franko',
+  movements: [100, 200, 300, 45],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
   },
 };
 
-console.log(employee.getWage());
+console.log(account.latest);
+account.latest = 50;
+
+console.log(account.movements);
+console.log(account.latest);
+
+const walter = new PersonCl('Walter a', 2005);
+console.log(walter.fullName);
